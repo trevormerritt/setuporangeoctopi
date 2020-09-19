af62aa141ad9804a7f04940677d440c5708b331e
Trevor Merritt <trevor.merritt@gmail.com>
2020-09-19 19:08:01 +0000

    Updates for 20200919

--- setup.sh
+++ setup.sh
@@ -4,16 +4,32 @@ INSTALL_BASE=/home/pi/
 CODE_BASE=`pwd`
 VIRTUALENV='/usr/bin/python /usr/lib/python2.7/dist-packages/virtualenv.py'
 OUTPUT_FILE='setup.log'
-SCRIPT_VERSION=0.0.1
+SCRIPT_VERSION=0.1.1
 SCRIPT_NAME='SetupOrangeOctoPrint'
+SCRIPT_DATE="20200919"
+SIGNAL_CLI_VERSION="0.6.10"
 
-if [[ $UID -ne 0 ]]; then
-  sudo -p 'Restarting as root, password: ' bash $0 "$@"
-  exit $?
-fi
+declare -A PLUGINS
+
+PLUGINS["ThermalRunaway"]="https://github.com/AlexVerrico/Octoprint-ThermalRunaway/archive/master.zip"
+PLUGINS["ConsolidatedTabs"]="https://github.com/jneilliii/OctoPrint-ConsolidatedTabs/archive/master.zip"
+PLUGINS["DeltaMicroCalibrator"]="https://github.com/Fabi0San/DuCalibrator/archive/master.zip"
+PLUGINS["SimpleFileManager"]="https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip"
+PLUGINS["ConsolicatedTempControl"]="https://github.com/jneilliii/OctoPrint-ConsolidateTempControl/archive/master.zip"
+PLUGINS["BedVisualizer"]="https://github.com/jneilliii/OctoPrint-BedLevelVisualizer/archive/master.zip"
+PLUGINS["AutoSelectUploaded"]="https://github.com/OctoPrint/OctoPrint-Autoselect/archive/master.zip"
+PLUGINS["FullScreenWebcam"]="https://github.com/BillyBlaze/OctoPrint-FullScreen/archive/master.zip"
+PLUGINS["HeaterTimeout"]="https://github.com/tjjfvi/OctoPrint-BetterHeaterTimeout/archive/master.zip"
+PLUGINS["HeaterFailsafe"]="https://github.com/google/OctoPrint-TemperatureFailsafe/archive/master.zip"
+PLUGINS["MultipleUpload"]="https://github.com/eyal0/OctoPrint-MultipleUpload/archive/master.zip"
+PLUGINS["SignalNotifications"]="https://github.com/aerickson/OctoPrint_Signal-Notifier/archive/master.zip"
+PLUGINS["Preheat"]="https://github.com/marian42/octoprint-preheat/archive/master.zip"
+PLUGINS["RequestSpinner"]="https://github.com/OctoPrint/OctoPrint-RequestSpinner/archive/master.zip"
+PLUGINS["SlackNotifications"]="https://github.com/richjoyce/OctoPrint-Slack/archive/master.zip"
+PLUGINS["OctoKlipper"]="https://github.com/AliceGrey/OctoprintKlipperPlugin/archive/master.zip"
 
 function usage() {
-  echo "$SCRIPT_NAME v$SCRIPT_VERSION"
+  echo "$SCRIPT_NAME v$SCRIPT_VERSION ($SCRIPT_DATE)"
   echo "$0"
   echo " --help (This)"
   echo " --restart-everything (Nuke Everything)"
@@ -28,84 +44,78 @@ function echo_green(){
 
 function setup_user_pi() {
   echo_green "Setting up PI user"
-  usermod -a -G tty pi
-  usermod -a -G dialout pi
-  echo "pi ALL=NOPASSWD: /sbin/shutdown" > /etc/sudoers.d/octoprint-shutdown
+  sudo usermod -a -G tty pi
+  sudo usermod -a -G dialout pi
+  echo "pi ALL=(NOPASSWD: /sbin/shutdown)" > /etc/sudoers.d/octoprint-shutdown
   echo "pi ALL=NOPASSWD: /usr/sbin/service" > /etc/sudoers.d/octoprint-service
 }
 
 function apt_steps() {
   echo_green "--Updating Cache"
-  apt-get update -y &> $OUTPUT_FILE
+  sudo apt-get update -y &> $OUTPUT_FILE
 
   echo_green "Removing Extras"
-  apt-get remove -y --purge scratch squeak-plugins-scratch squeak-vm \
+  sudo apt-get remove -y --purge scratch squeak-plugins-scratch squeak-vm \
    wolfram-engine python-minecraftpi minecraft-pi sonic-pi oracle-java8-jdk \
    bluej libreoffice-common libreoffice-core freepats greenfoot \
    nodered &> $OUTPUT_FILE
 
   echo_green "--Adding support software for Octoprint"
-  apt-get -y --force-yes install python2.7 python-virtualenv python-dev \
+  sudo apt-get -y --force-yes install python2.7 python-virtualenv python-dev \
     git screen subversion cmake checkinstall avahi-daemon \
     libavahi-compat-libdnssd1 libffi-dev libssl-dev libjpeg62-turbo-dev \
-    ssl-cert haproxy cmake &> $OUTPUT_FILE
-  apt-get install --reinstall iputils-ping &> $OUTPUT_FILE
-  apt-get -y --force-yes --no-install-recommends install imagemagick \
+    ssl-cert haproxy cmake pipx python3-venv python3-dev libpython3-dev \
+    python3-wheel python-wheel-common &> $OUTPUT_FILE
+  sudo apt-get install --reinstall iputils-ping &> $OUTPUT_FILE
+  sudo apt-get -y --force-yes --no-install-recommends install imagemagick \
     libav-tools libv4l-dev &> $OUTPUT_FILE
 
   echo_green "--Updating Software"
-  apt-get -y upgrade &> $OUTPUT_FILE
+  sudo apt-get -y upgrade &> $OUTPUT_FILE
 }
 
 function cleanup() {
   echo_green "Cleaning up Space"
-  apt-get -y clean &> $OUTPUT_FILE
+  sudo apt-get -y clean &> $OUTPUT_FILE
   echo_green "--Removing no longer needed packages"
-  apt-get -y autoremove &> $OUTPUT_FILE
+  sudo apt-get -y autoremove &> $OUTPUT_FILE
 }
 
 function setup_venv() {
-  echo_green "Setting up virtualenv and pip"
+  echo_green "Setting up pipx"
   cd $INSTALL_BASE
-  sudo -u pi $VIRTUALENV oprint &> $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install --upgrade pip &> $OUTPUT_FILE
 }
 
 function setup_octoprint() {
-  echo_green "Downloading Octoprint"
+  echo_green "Settings up Octoprint"
   cd $INSTALL_BASE
-  git clone https://github.com/foosel/OctoPrint.git OctoPrint &> $OUTPUT_FILE
-  chown -R pi:pi OctoPrint
-  cd OctoPrint
-  echo_green "--Building Octoprint"
-  sudo -u pi $INSTALL_BASE/oprint/bin/python setup.py install &> $OUTPUT_FILE
-  echo_green "--Setting up Octoprint in Environment"
-  cd ..
-  cp $CODE_BASE/files/etc_initd_octoprint /etc/init.d/octoprint
-  cp $CODE_BASE/files/etc_default_octoprint /etc/default/octoprint
-  chmod +x /etc/init.d/octoprint
-  update-rc.d octoprint defaults 95
+  pipx ensurepath
+  pipx install octoprint
+  pipx inject octoprint wheel
+  echo_green "--Setting up Auto-Start and Defaults"
+  sudo cp $CODE_BASE/files/etc_initd_octoprint /etc/init.d/octoprint
+  sudo cp $CODE_BASE/files/etc_default_octoprint /etc/default/octoprint
+  sudo chmod +x /etc/init.d/octoprint
+  sudo update-rc.d octoprint defaults 95
 }
 
 function setup_octoprint_plugins() {
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/OctoPrint/OctoPrint-Autoselect/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/OctoPrint/OctoPrint-DisplayProgress/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/jasiek/OctoPrint-Cost/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/dattas/OctoPrint-DetailedProgress/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/kantlivelong/OctoPrint-PSUControl/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/malnvenshorn/OctoPrint-CostEstimation/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/google/OctoPrint-HeaterTimeout/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/marian42/octoprint-preheat/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/pablogventura/Octoprint-ETA/archive/master.zip" > $OUTPUT_FILE
-  sudo -u pi $INSTALL_BASE/oprint/bin/pip install "https://github.com/OctoPrint/OctoPrint-FirmwareUpdater/archive/master.zip" > $OUTPUT_FILE
+
+for key in ${!PLUGINS[@]}; do
+    echo Downloading ${key}
+    # curl -L https://github.com/AliceGrey/OctoprintKlipperPlugin/archive/master.zip > OctoprintKlipper.zip
+    curl -L ${PLUGINS[${key}]} > ${key}.zip
+    echo Installing
+    pipx inject octoprint ${key}.zip
+done
+
 }
 
 function setup_mjpg_streamer() {
   echo_green "Setting up mjpg_streamer"
   cd $INSTALL_BASE
   git clone https://github.com/jacksonliam/mjpg-streamer.git mjpg-streamer > $OUTPUT_FILE
-  chown -R pi:pi mjpg-streamer
+  sudo chown -R pi:pi mjpg-streamer
   cd mjpg-streamer
   mv mjpg-streamer-experimental/* .
   echo_green "--Building binaries"
@@ -121,21 +131,32 @@ function setup_mjpg_streamer() {
     rm /etc/ssl/certs/ssl-cert-snakeoil.pem
   fi
 
-  cp $CODE_BASE/files/mjpg_www_index /home/pi/mjpg-streamer/www-octopi/index.html
-  cp $CODE_BASE/files/etc_initd_webcamd /etc/init.d/webcamd
-  cp $CODE_BASE/files/etc_default_webcamd /etc/default/webcamd
-  mkdir /root/bin
-  cp $CODE_BASE/files/root_bin_webcamd /root/bin/webcamd
-  chmod +x /root/bin/webcamd
-  chmod +x /etc/init.d/webcamd
-  update-rc.d webcamd defaults
+  sudo cp $CODE_BASE/files/mjpg_www_index /home/pi/mjpg-streamer/www-octopi/index.html
+  sudo cp $CODE_BASE/files/etc_initd_webcamd /etc/init.d/webcamd
+  sudo cp $CODE_BASE/files/etc_default_webcamd /etc/default/webcamd
+  sudo mkdir /root/bin
+  sudo cp $CODE_BASE/files/root_bin_webcamd /root/bin/webcamd
+  sudo chmod +x /root/bin/webcamd
+  sudo chmod +x /etc/init.d/webcamd
+  sudo update-rc.d webcamd defaults
+
+}
+
+function setup_signal_support() {
+  echo_green "Installing Signal Support"
+  cd $CODE_BASE
+  mkdir signal-cli
+  cd signal-cli
+  wget https://github.com/AsamK/signal-cli/releases/download/v"${SIGNAL_CLI_VERSION}"/signal-cli-"${SIGNAL_CLI_VERSION}".tar.gz
+  sudo tar xf signal-cli-"${SIGNAL_CLI_VERSION}".tar.gz -C /opt
+  sudo ln -sf /opt/signal-cli-"${SIGNAL_CLI_VERSION}"/bin/signal-cli /usr/local/bin/
 
 }
 
 function setup_haproxy() {
   echo_green "Building HAProxy configuration"
   cp $CODE_BASE/files/etc_haproxy_haproxycfg /etc/haproxy/haproxy.cfg
-  service haproxy restart
+  sudo service haproxy restart
 }
 
 while test $# -gt 0
@@ -174,21 +195,21 @@ fi
 if [ -n "$DO_INSTALL" ]
 then
 
-  clear
-  echo_green "Starting to install from $CODE_BASE to $INSTALL_BASE..."
-  apt_steps
-  echo_green "...Install of packages complete"
-  echo_green "Fixing SSH"
-  echo "IPQoS 0x00" >> /etc/ssh/sshd_config
+#  clear
+#  echo_green "Starting to install from $CODE_BASE to $INSTALL_BASE..."
+#  apt_steps
+#  echo_green "...Install of packages complete"
+#  echo_green "Fixing SSH"
+#  echo "IPQoS 0x00" >> /etc/ssh/sshd_config
 
-  setup_user_pi
-  setup_venv
-  setup_octoprint
-  setup_octoprint_plugins
-  setup_mjpg_streamer
-  setup_haproxy
-  cleanup
-  shutdown now -r
+#  setup_user_pi
+#  setup_venv
+setup_octoprint
+setup_octoprint_plugins
+#  setup_mjpg_streamer
+#  setup_haproxy
+#  cleanup
+  # shutdown now -r
 else
   usage
 fi
